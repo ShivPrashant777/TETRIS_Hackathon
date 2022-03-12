406f4ddc18e25237c3f79cc4f7b535c3323d1490
ShivPrashant777 <shivprashant777@gmail.com>
2022-03-13 01:41:46 +0530

    dashboard sidebar update

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,44 +1,14 @@
 import React from 'react'
-import CollegeLogo from '../Institutes/Logo_of_G.H._Raisoni_College_of_Engineering_Nagpur.png'
 import './Dashboard.css'
 import TopRecruiters from './TopRecruiters'
 import History from './History'
 import PlacementsDoughnut from './PlacementsDoughnut'
+import Sidebar from './Sidebar'
 
 const Dashboard = () => {
     return (
-        <div className="container-fluid row">
-            <div className="col-3 px-5 bg-white subtle-shadow">
-                <h1>Navrohan</h1>
-                <div className="d-flex flex-column justify-content-center .align-items-center">
-                    <div className="institute_img text-center">
-                        <img src={CollegeLogo} alt="" width="150px" />
-                    </div>
-                    <div className="institute_img text-center">
-                        <b> G.H. Raisoni College of Engnieering</b>
-                    </div>
-                    <div className="institute_img text-center">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Illo veritatis dolore fugiat quasi, repellendus,
-                        recusandae a, quos omnis sunt laudantium harum ratione
-                        labore nihil doloribus praesentium quo corporis
-                        excepturi enim.
-                    </div>
-
-                    <div className="text-start">
-                        <div className="border my-2 p-2 rounded highlighted">
-                            Dashboard
-                        </div>
-                        <div className="border my-2 p-2 rounded">Home</div>
-                        <div className="border my-2 p-2 rounded">
-                            Institutes
-                        </div>
-                        <div className="border my-2 p-2 rounded">Manage</div>
-                        <div className="border my-2 p-2 rounded">Listings</div>
-                        <div className="border my-2 p-2 rounded">About Us</div>
-                    </div>
-                </div>
-            </div>
+        <div className="container-fluid row dashboard-container">
+            <Sidebar />
             <div className="col-9 py-4 px-5">
                 <h3>Dashboard</h3>
                 <TopRecruiters />
---
+++ client/src/components/dashboard/Sidebar.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import CollegeLogo from '../Institutes/Logo_of_G.H._Raisoni_College_of_Engineering_Nagpur.png'
+import NavrohanLogo from '../layout/Navrohan_Logo.png'
+
+const Sidebar = () => {
+    return (
+        <div className="col-3 py-2 px-2 px-xl-5 bg-white subtle-shadow sidebar">
+            <div className="d-flex flex-column justify-content-center">
+                <div className="text-center">
+                    <img src={NavrohanLogo} alt="" width="150px" />
+                </div>
+                <div className="institute_img text-center my-4">
+                    <img src={CollegeLogo} alt="" width="150px" />
+                </div>
+                <div className="text-center text-secondary">
+                    <h5>
+                        <b> G.H. Raisoni College of Engnieering</b>
+                    </h5>
+                </div>
+                <div className="text-center college-info d-flex flex-column my-2">
+                    <div className="d-flex justify-content-between">
+                        <p>College Type</p>
+                        <p>:</p>
+                        <p>Autonomous</p>
+                    </div>
+                    <div className="d-flex justify-content-between text-left">
+                        <p>Location</p>
+                        <p>:</p>
+                        <p>Nagpur, Maharashtra</p>
+                    </div>
+                    <div className="d-flex justify-content-between text-left">
+                        <p>President</p>
+                        <p>:</p>
+                        <p>Sunil Raisoni</p>
+                    </div>
+                    <div className="d-flex justify-content-between text-left">
+                        <p>Contact</p>
+                        <p>:</p>
+                        <p>9988776654</p>
+                    </div>
+                </div>
+
+                <div className="text-start my-2">
+                    <div className="border my-2 p-2 rounded highlighted">
+                        Dashboard
+                    </div>
+                    <div className="my-2 m p-2 rounded">Home</div>
+
+                    <div className="my-2 p-2 rounded">Institutes</div>
+                    <div className="my-2 p-2 rounded">Manage</div>
+                    <div className="my-2 p-2 rounded">Listings</div>
+                </div>
+                <div className="account">
+                    <div className="d-flex justify-content-start align-items-center">
+                        <img src={CollegeLogo} alt="" width="40px" />
+                        <div style={{margin: '0 5px', fontSize: '0.8em'}}>
+                            <b>GHRCE</b>
+                            <p className="light-text" style={{margin: 0}}>
+                                Account details
+                            </p>
+                        </div>
+                        <span className="chevron bottom ml-auto"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
